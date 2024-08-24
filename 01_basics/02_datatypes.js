823580bdce336d6324827295ebbbad45c850cb92
divyanshivats <151052819+divyanshivats@users.noreply.github.com>
2024-08-24 11:11:34 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+// "use strcit"; // treat all js  oode as newer version of js - it is not necessary to write
+
+// alert() // works on browser
+
+// console.log(3+3); console.log("hello"); -- readable nhi h code though it will work
+
+let name = "JAvaScript"
+let age = 12
+let isLoggedIn = false
+
+
+
+// number
+// bigInt 
+// string ""
+// boolean true/false
+// null => standalone value in js - represntation of empty value
+let temp = null // null
+// undefined => value isnt defined
+let x; // undefined
+
+//symbol => to find unique-ness
+
+
+//object 
+
+console.log(typeof age)
+console.log(typeof temp) // object - null
+console.log(typeof x)
+
+/* 
+null => is an object 
+undefined is a type
+
+*/
