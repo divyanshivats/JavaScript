d859424297f6ead558b6904f57b574e2b3daae3f
divyanshivats <151052819+divyanshivats@users.noreply.github.com>
2024-08-28 07:29:46 +0000

    operations

---
+++ 01_basics/03_Operations.js
@@ -0,0 +1,55 @@
+// ****************** OPERATIONS ***************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2**3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = "hitesh"
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + "2" + 2);
+// console.log(1 + 2 + "3");
+
+
+// although these conversion are not optimal to use - 
+console.log(true); 
+console.log(+true); //1
+
+console.log(+""); //0
+
+
+
+// ASSINGMENT -
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2+2
+// console.log(num1);
+// console.log(num2);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
