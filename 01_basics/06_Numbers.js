dca98f0b748729f738a88beac46590c776eaef59
divyanshivats <151052819+divyanshivats@users.noreply.github.com>
2024-08-29 11:50:06 +0000

    Numbers & Math

---
+++ 01_basics/06_Numbers.js
@@ -0,0 +1,41 @@
+// const score = 400
+
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(score);
+
+// console.log(typeof balance); //object
+// console.log(typeof score); //number
+
+// console.log(balance.toString())
+// console.log(balance.toFixed(2)); // fix the precision value
+
+// const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(1));
+// console.log(otherNumber.toPrecision(2));
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+/* *************** MATHS ************** */
+
+console.log(Math); // gives more detailed explanation at console
+console.log(Math.abs(-4));
+console.log(Math.round(5.8));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.max(3,75,32,425))
+
+console.log(Math.random()); //gives bw 0-1
+console.log(Math.random()*10);
+
+// if value comes as 0.004 then to avoid such cases we use
+console.log((Math.random()*10) +1);
+
+
+
+
+
