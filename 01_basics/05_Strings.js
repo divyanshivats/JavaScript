c398b922fb246a445a6757a060bc3e16030155d6
divyanshivats <151052819+divyanshivats@users.noreply.github.com>
2024-08-29 11:49:37 +0000

    Strings

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,60 @@
+// const name = "stefan"
+// const lastName = "salvatore"
+// const repoCount = 23
+
+// console.log(name + lastName);
+// console.log(name + repoCount + "value");
+
+
+// console.log(`hello my name is ${name +" " + lastName} and my repo count is ${ repoCount}`);
+
+// const gameName = new String('codingWorld') 
+// // in console it gives indexing and prototypes many methods 
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__); // here it shows empty but in console it will give many
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('o'));
+
+
+// const gameName = new String('coding-World');
+// const newString = gameName.substring(0, 7);
+// console.log(newString);
+
+// // const gameName2 = new String('codingWorld'); 
+// // // Corrected variable name to avoid redeclaration issues.
+// // console.log(gameName2);
+
+// const another = gameName.slice(0,4)
+// console.log(another);
+
+// const another2 = gameName.slice(-9,4)
+// console.log(another2);
+
+
+
+const example = " hello  "
+console.log(example);
+console.log(example.trim()); // to remove leading and trailing spaces 
+
+
+const url = "https://coding.com/javascript%20okok"
+console.log(url.replace("%20okok", "strings.js"));
+
+console.log(url.includes("javascript"));
+
+
+// to convert string into array based on smt (spaces, dash etc)
+const game = "hello-java-script"
+console.log(game.split('-'));
+
+
+
+
+
+
+
+
