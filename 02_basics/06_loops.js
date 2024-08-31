532d38efd0642bad04f31f4c8b00deb3d7f059f9
divyanshivats <151052819+divyanshivats@users.noreply.github.com>
2024-08-31 17:52:34 +0000

    Loops

---
+++ 02_basics/06_loops.js
@@ -0,0 +1,17 @@
+for (let index = 0; index <= 10; index++) {
+    console.log(index);
+}
+
+let index =0;
+while (index<=10) {
+    console.log(index);
+    index++;
+    
+}
+
+score =0
+do {
+    console.log(score);
+    score++;
+    
+} while (score<=10);
